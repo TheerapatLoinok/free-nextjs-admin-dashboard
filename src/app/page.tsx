b12d66a099a4e382d901adefb77c1517bcae4121
theerapatloinok <theerapat.l@kkumail.com>
2024-10-05 13:52:41 +0700

    init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  title: "Trustfinance sprint report",
   description: "This is Next.js Home for TailAdmin Dashboard Template",
+  icons: {
+    icon: "/images/tfn-logo-favicon.png",
+  },
 };
 
 export default function Home() {
